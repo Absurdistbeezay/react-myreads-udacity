7010f3fbffc910c49e873a6835dc510781fcb835
Vijay Lama <b_jay05@hotmail.com>
2018-10-17 02:09:30 -0400

    main and search component initial setup

---
+++ src/components/MainPage.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export class MainPage extends Component {
+  render() {
+    return <div>Main Page</div>;
+  }
+}
+
+export default MainPage;
---
+++ src/components/SearchPage.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export class SearchPage extends Component {
+  render() {
+    return <div>SearchPage</div>;
+  }
+}
+
+export default SearchPage;
